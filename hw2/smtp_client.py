25f084a8e090928cd1404fb16a0c2f64bc5cc77a
Sam Balana <sambalana247@gmail.com>
2017-10-31 12:59:01 -0700

    feat(hw2): Send SMTP commands

--- hw2/smtp_client.py
+++ hw2/smtp_client.py
@@ -11,21 +11,34 @@ clientSocket.connect((mailserver, 25))
 response = clientSocket.recv(1024).decode()
 print(response)
 
-if response[:3] != '220':
-    print('220 reply not received from server.')
+def send(command):
+    clientSocket.send(command.encode())
+    response = clientSocket.recv(1024).decode()
+    print(response)
+    return response
+
+def expect(command, expectedCode):
+    response = send(command)
+    if response[:3] != str(expectedCode):
+        print(str(expectedCode) + ' reply not received from server.')
 
 # Send HELO command
-heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand.encode())
-response = clientSocket.recv(1024).decode()
-print(response)
-
-if response[:3] != '250':
-    print('250 reply not received from server.')
+expect('HELO Alice\r\n', 250)
 
 # Send MAIL FROM command
+expect('MAIL FROM: <sbalana@uci.edu>\r\n', 250)
+
 # Send RCPT TO command
+expect('RCPT TO: <sbalana@uci.edu>\r\n', 250)
+
 # Send DATA command
+expect('DATA\r\n', 354)
+
 # Send message data
+send(message)
+
 # Message ends with a single period
+expect(closeSequence, 250)
+
 # Send QUIT command
+expect('QUIT\r\n', 221)
